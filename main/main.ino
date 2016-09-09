a08a92d16d019ed58fc9ee087127c177fe980591
sne3ks <lenselle.nicolas@gmail.com>
2016-09-09 16:14:45 +0400

    remove test code

--- main/main.ino
+++ main/main.ino
@@ -3,17 +3,12 @@
 #include "instructions/adaServoShield_set.h"
 #include "instructions/lib/AccelStepper.h"
 
-
-AccelStepper s(AccelStepper::FULL4WIRE, 9, 10, 11, 12);
-
 void setup() {
   _exode.setup();
   load_exode_instructions(0);
-  load_adaServoShield_instructions(1);
 }
 
 void loop() {
   // Enjoy it !
  _exode.run();
 }
-
